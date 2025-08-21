70669fa6e57faf24f67c40fa74e996440f21c46e
franns008 <franns008@gmail.com>
2025-08-21 15:58:42 -0300

    Se hace hasta el ejercicio 4

--- src/Practica1/Ejercicio4/Logger.java
+++ src/Practica1/Ejercicio4/Logger.java
@@ -1,5 +1,28 @@
 package Practica1.Ejercicio4;
 
 public class Logger {
+    private static Logger instancia;
 
+    private Logger(){
+
+    }
+
+    public static Logger  getLogger(){
+        if(Logger.instancia==null){
+            instancia=new Logger();
+        }
+        return instancia;
+    }
+
+    public void logInfo(String mensaje){
+        System.out.println("{info} "+mensaje);
+    }
+
+    public void logError(String mensaje){
+        System.out.println("{error} "+mensaje);
+    }
+
+    public void logWarning(String mensaje){
+        System.out.println("{warning} "+mensaje);
+    }
 }
