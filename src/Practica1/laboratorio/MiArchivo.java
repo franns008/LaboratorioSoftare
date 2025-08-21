837feca036e8ca95b926ac23c234a1775e84692f
franns008 <franns008@gmail.com>
2025-08-21 10:02:40 -0300

    Se hace hasta el ejercicio 3.2

---
+++ src/Practica1/laboratorio/MiArchivo.java
@@ -0,0 +1,9 @@
+package Practica1.laboratorio;
+import java.io.File;
+
+public class MiArchivo extends File {
+    public MiArchivo() {
+        super("Este es un pathName");
+        System.out.println("Mi Archivo instanciado") ;
+    }
+}
